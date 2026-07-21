f3928a241f557ed8669b59bfb425be58eb21d893
Sukrut Bilaskar <bsukrut407@gmail.com>
2026-07-21 15:26:59 +0530

    feat(document-service): bootstrap Spring Boot service with PostgreSQL

---
+++ services/document-service/src/test/java/com/clauseiq/document/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clauseiq.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
